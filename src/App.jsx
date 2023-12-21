74184825f4cbec0b5568646a31bfcdebda8b7759
rtsdeveloper <sharmaritesh9794@gmail.com>
2023-12-21 17:27:28 +0530

    to-do

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [data, setdata] = useState({ name: "", value: "Assign" });
+    const [record, setRecord] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>add to do</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const handleChange = (evt) => {
+        setdata({ ...data, [evt.target.name]: evt.target.value });
+    };
 
-export default App
+    const handleClick = () => {
+        if (data.name === "") {
+            alert("Kindly Fill Task in it.");
+        } else {
+            const rcds = [...record, { ...data }];
+            setRecord(rcds);
+            setdata({ name: "", value: "Assign" });
+        }
+    };
+
+    const handleDel = (j) => {
+        const rcords = record.filter((_, i) => i !== j);
+        setRecord(rcords);
+    };
+
+    const handleToggle = (j) => {
+        const updatedRecord = [...record];
+        updatedRecord[j].value = updatedRecord[j].value === "Assign" ? "Completed" : "Assign";
+        setRecord(updatedRecord);
+    };
+
+    return (
+        <div className="p-5">
+            <h4>Todo Task</h4>
+            <div className="mb-3">
+                <input type="text" name="name" value={data.name} className="form-control-sm" onChange={handleChange} />
+            </div>
+            <input type="button" value="Add" onClick={handleClick} className="btn btn-primary" />
+            {record.map((task, j) => (
+                <div className="d-flex  mb-3 mt-3 " key={j}>
+                    <div onClick={() => handleDel(j)} className="col-8">
+                        <h4>
+                            {j + 1}. {task.name}
+                        </h4>
+                    </div>
+                    <div className="col-4" onClick={() => handleToggle(j)}>
+                        <input type="button" className={task.value === "Assign" ? "btn btn-primary" : "btn btn-success"} value={task.value} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default App;
