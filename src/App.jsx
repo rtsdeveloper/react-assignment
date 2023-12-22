6e6fffde090ea4403b6bd250b077dc3d65cad483
rtsdeveloper <sharmaritesh9794@gmail.com>
2023-12-22 13:46:24 +0530

    new update of Boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello this is Main Branch...!</h1>
+      <h1>This is Main Branch</h1>
     </div>
   )
 }
 
 export default App
+
