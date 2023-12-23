66805945a54013d6bc4c52dc871f6f055d57b874
rtsdeveloper <sharmaritesh9794@gmail.com>
2023-12-23 17:25:01 +0530

    useless

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>This is Main Branch</h1>
+      <h1>This is Main Directory</h1>
     </div>
   )
 }
 
 export default App
-
