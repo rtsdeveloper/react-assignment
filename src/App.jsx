d4ec9fa0410314859501db1be59c2f3e26ee8d2d
rtsdeveloper <sharmaritesh9794@gmail.com>
2023-12-26 11:24:16 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 
 const App = () => {
-  
-  const [data, setdata] = useState([{ name: "", value: "Assign" }]);
+  const [data, setData] = useState({ name: "", value: "Assign" });
   const [record, setRecord] = useState([]);
 
   const handleChange = (e) => {
-    setdata({ ...data, [e.target.name]: e.target.value});
+    setData({ ...data, [e.target.name]: e.target.value });
   };
-  
+
   const handleClick = () => {
     if (data.name.trim() === "") {
       alert("Kindly Fill Task in it.");
-      
     } else {
-      const rcds = [...record, { ...data }];
+      const rcds= [...record, { ...data }];
       setRecord(rcds);
-      setdata({ name: "", value: "Assign" });
+      setData({ name: "", value: "Assign" });
     }
   };
 
   const handleDel = (j) => {
     const rcords = record.filter((_, i) => i !== j);
-    console.log(rcords);
     setRecord(rcords);
   };
-
-  const handleToggle = (j) => {
+  
+  const handleCheckbox = (j) => {
     const updatedRecord = [...record];
     updatedRecord[j].value = updatedRecord[j].value === "Assign" ? "Completed" : "Assign";
     setRecord(updatedRecord);
@@ -36,27 +33,66 @@ const App = () => {
     <div className="p-5">
       <h4>Todo Task</h4>
       <div className="mb-3">
-        <input type="text" name="name" value={data.name} className="form-control-sm" onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          value={data.name}
+          className="form-control-sm"
+          onChange={handleChange}
+        />
       </div>
-      <input type="button" value="Add" onClick={handleClick} className="btn btn-primary" />
+      <input
+        type="button"
+        value="Add"
+        onClick={handleClick}
+        className="btn btn-primary"
+      />
       {record.map((task, j) => (
-        <div className="d-flex  mb-3 mt-3 " key={j}>
-          <div className="col-8"  >
+        <div className="d-flex mb-3 mt-3" key={j}>
+          <div className="col-8">
             <span>
-              <input style={{ cursor: "pointer" }} type="checkbox" className="form-check-input me-2" onClick={() => handleToggle(j)} />
-              <span className="h5">{j + 1}.</span> </span>
-            <span className={task.value == 'Assign' ? "h5" : "text-danger text-decoration-line-through h5"} >
+              <input
+                style={{ cursor: "pointer" }}
+                type="checkbox"
+                className="form-check-input me-2"
+                onClick={() => handleCheckbox(j)}
+              />
+              <span className="h5">{j + 1}.</span>{" "}
+            </span>
+            <span
+              className={
+                task.value === "Assign"
+                  ? "h5"
+                  : "text-danger text-decoration-line-through h5"
+              }
+            >
               {task.name}
             </span>
           </div>
           <div className="col-4 d-flex">
-            <div className="col-6" >
-              <span className={task.value === "Assign" ? "badge bg-primary" : "badge bg-success"}>
+            <div className="col-6">
+              <span
+                className={
+                  task.value === "Assign"
+                    ? "badge bg-primary"
+                    : "badge bg-success"
+                }
+              >
                 {task.value === "Assign" ? "New" : "Completed"}
               </span>
             </div>
-            <div className="col-6 d-flex justify-content-center align-items-center" onClick={() => handleDel(j)}>
-              <i style={{ cursor: "pointer", fontSize: "25px", color: "red" }} className="fa-solid fa-trash-can"></i>
+            <div
+              className="col-6 d-flex justify-content-center align-items-center"
+              onClick={() => handleDel(j)}
+            >
+              <i
+                style={{
+                  cursor: "pointer",
+                  fontSize: "25px",
+                  color: "red"
+                }}
+                className="fa-solid fa-trash-can"
+              ></i>
             </div>
           </div>
         </div>
